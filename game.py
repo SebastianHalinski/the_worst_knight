4f6c32039ea232ab23d8a06e231a55234b4c38a0
Sebastian <sebastian.halinski@gmail.com>
2017-09-06 11:41:44 +0200

    add maps, move function

--- game.py
+++ game.py
@@ -1,3 +1,75 @@
+import os
+
+def getch():
+    import sys, tty, termios
+    fd = sys.stdin.fileno()
+    old_settings = termios.tcgetattr(fd)
+    try:
+        tty.setraw(sys.stdin.fileno())
+        ch = sys.stdin.read(1)
+    finally:
+        termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+    return ch
+
+
+
+def map_1():
+    path = r'/home/sebastian/Dokumenty/The_worst_knight/map1.txt'
+    file_name = open(path, "r")
+    map_1_list = []
+    for map_1_row in file_name:
+        for i in map_1_row:
+            map_1_list.append(i)
+    return map_1_list
+
+
+def map_2():
+    path = r'/home/sebastian/Dokumenty/The_worst_knight/map2.txt'
+    file_name = open(path, "r")
+    map_1_list = []
+    for map_1_row in file_name:
+        for i in map_1_row:
+            map_1_list.append(i)
+    return map_1_list
+
+
+def move(map_1_list):
+    start_position = 73
+    left_right = 1
+    up_down = 71
+
+    while True:
+        print(''.join(map_1_list))
+        i = getch()
+
+        if i == "a" and map_1_list[start_position - left_right] == '.':
+            os.system('clear')
+            map_1_list[start_position - left_right] = '@'
+            map_1_list[start_position] = "."
+            start_position -= left_right
+        elif i == "d" and map_1_list[start_position + left_right] == '.':
+            os.system('clear')
+            map_1_list[start_position + left_right] = '@'
+            map_1_list[start_position] = "."
+            start_position += left_right
+        elif i == "w" and map_1_list[start_position - up_down] == '.':
+            os.system('clear')
+            map_1_list[start_position - up_down] = '@'
+            map_1_list[start_position] = "."
+            start_position -= up_down
+        elif i == "s" and map_1_list[start_position + up_down] == '.':
+            os.system('clear')
+            map_1_list[start_position + up_down] = '@'
+            map_1_list[start_position] = "."
+            start_position += up_down
+        elif i == "q":
+            break
+        else:
+            os.system('clear')
+
+
+
+
 def welcome_story (user_name):
     print ("Hello", user_name)
     story=open(r"C:\Users\Marta\Documents\codecool\python\game_knight\the_worst_knight\story.txt").read()
@@ -9,33 +81,21 @@ def welcome_story (user_name):
         print ("You will die anyway")
     else:
         welcome_story(user_name)
-    
 
-def first_level():
-    width = 70
-    height = 15
-    board = []
-    element_full = []
-    for i in range(width):
-        element_full.append("X")
-    element_blanks = []
 
-    element_blanks.append("X")
-    for i in range(width - 2):
-        element_blanks.append(" ")
-    element_blanks.append("X")
-    board.append(element_full)
 
-    for i in range(height - 2):
-        board.append(element_blanks)
-    board.append(element_full)
-
-    for n in board:
-       print(''.join(n))
-    pass
-
-def main ():
+def main():
     user_name = input ("Enter your name: ")
     welcome_story(user_name)
+    map_1_list = map_1()
+    print('press C to start')
+    if getch() == 'c':
+        os.system('clear')
+        move(map_1_list)
+    map_1_list = map_2()
+    move(map_1_list)
 
-main ()
+main()
+
+
+    
